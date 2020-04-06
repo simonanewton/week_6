29866283c0111fa3bdea50d017c80582842d41e3
Simon Newton <simonanewton@gmail.com>
2020-04-06 09:08:28 -0400

    1.4 added inital forecast functionality

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -2,16 +2,16 @@ $(document).ready(function () {
 
     var displayCity = $("#display-city");
     var displayDate = $("#display-date");
-    var displayIcon =  $("#display-icon");
+    var displayIcon = $("#display-icon");
 
     var displayTemp = $("#display-temp");
     var displayHumid = $("#display-humid");
     var displayWind = $("#display-wind");
     var displayIndex = $("#display-index");
 
-    var currentDate = moment().format('l');
-
     var APIkey = "5127b984f77556497cf3b63325863b1a";
+
+    var currentDate = moment().format('l');
 
     // -----------------------------------------------------------------------------------
 
@@ -23,21 +23,21 @@ $(document).ready(function () {
     function addIcon(condition) {
         switch (condition) {
             case "Clear":
-                return "fas fa-sun fa-2x px-2";
+                return "fas fa-sun";
             case "Clouds":
-                return "fas fa-cloud fa-2x px-2";
+                return "fas fa-cloud";
             case "Rain":
-                return "fas fa-cloud-rain fa-2x px-2";
+                return "fas fa-cloud-rain";
             case "Snow":
-                return "far fa-snowflake fa-2x px-2";
+                return "far fa-snowflake";
             case "Mist":
-                return "fas fa-smog fa-2x px-2"
+                return "fas fa-smog"
             default:
-                return "fas fa-cloud-sun fa-2x px-2";
+                return "fas fa-cloud-sun";
         }
     }
 
-    function displayUVIndex(lat,lon) {
+    function displayUVIndex(lat, lon) {
         var queryURL = `http://api.openweathermap.org/data/2.5/uvi?appid=${APIkey}&lat=${lat}&lon=${lon}`;
 
         $.ajax({
@@ -55,8 +55,6 @@ $(document).ready(function () {
             url: queryURL,
             method: "GET"
         }).then(function (response) {
-            console.log(response);
-
             displayCity.text(response.name);
             displayDate.text("(" + currentDate + ")");
             displayIcon.addClass(addIcon(response.weather[0].main));
@@ -71,12 +69,31 @@ $(document).ready(function () {
     function displayForecast(city) {
         queryURL = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIkey}`;
 
+        console.log(queryURL);
+
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
-            console.log("Response:");
-            console.log(response);
+            var day1 = response.list[7];
+            var day2 = response.list[15];
+            var day3 = response.list[23];
+            var day4 = response.list[31];
+            var day5 = response.list[39];
+
+            var forecastArray = [day1, day2, day3, day4, day5];
+
+            forecastArray.forEach(day => {
+                console.log(day);
+            });
+
+            for (var i = 0; i < forecastArray.length; i++) {
+                $(`#day-${i + 1}-icon`).addClass(addIcon(forecastArray[i].weather[0].main));
+                $(`#day-${i + 1}-temp`).text("Temperature: " + convertTemp(forecastArray[i].main.temp));
+                $(`#day-${i + 1}-humid`).text("Humidity: " + forecastArray[i].main.humidity);
+            }
+
+
         });
     }
 
